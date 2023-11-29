1d5e23688756290a1bc89613d6de21f926222bb1
Guillermo <guillermofabrod@gmial.com>
2023-11-29 19:36:33 +0100

    se me habia olvidado

--- Controladores/funciones.js
+++ Controladores/funciones.js
@@ -1,13 +1,4 @@
-// const subjects = [
-//     {
-//       name: 'David Choak',
-//       type: 'descargado',
-//       location: 'LakeView',
-//       culpability: 'delincuente',
-//       image: 'url_de_imagen',
-//       comments: 'Considerado líder de la banda'
-//     }
-//   ];
+
   
   function ira(ruta){
 
